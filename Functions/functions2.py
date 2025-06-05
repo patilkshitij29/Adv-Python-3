e8904245c47f414851e805fe15c5e67220151dfa
patilkshitij29 <patilkshitij29@gmail.com>
2025-06-05 10:20:33 +0530

    functions-introduction of args

---
+++ Functions/functions2.py
@@ -0,0 +1,15 @@
+# def add2(x,y):
+#     """
+#     returns addition of given input data
+#     """
+#     c1 = x+y
+#     return c1
+
+# a1 = add2(15,90)
+# print(a1)
+
+def multiply(x,y):
+    return x*y
+m1 = multiply(100,8)
+print(m1)
+print(multiply(50,5))
